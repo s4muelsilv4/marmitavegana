c2a13a318fa14ce7b6a0e1fecca3eea4d3619d86
Samuel Silva <samuelmathematical@gmail.com>
2021-09-07 14:13:59 -0300

    documentar base de dataset comida_vegana

---
+++ R/dataset.R
@@ -0,0 +1,15 @@
+#' Cardapio de comida vegana
+#'
+#' Caradapio de comida vegana disponivel no beleaf
+#'
+#' @format Um tibble com 39 linhas e 6 colunas:
+#' \describe{
+#'   \item{item}{Enumeracao de cada um dos pratos}
+#'   \item{nome}{Nome do prato}
+#'   \item{descricao}{Ingredientes utilizados na preparacao do prato}
+#'   \item{preco}{Preco em reais dos pratos}
+#'   \item{tipo_refeicao}{Especificacao da refeicao: prato, sopa e doce}
+#'   \item{url}{link para acesso do prato na web}
+#' }
+#' @source \url{"https://www.beleaf.com.br/cardapio-marmitas-veganas"}
+"marmita_vegana"
