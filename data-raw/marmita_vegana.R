00b13bf3477283024b0c7789c72192c5460aad9f
Samuel Silva <samuelmathematical@gmail.com>
2021-09-07 11:17:23 -0300

    criar data-raw e data

---
+++ data-raw/marmita_vegana.R
@@ -0,0 +1,33 @@
+## code to prepare `marmita_vegana` dataset goes here
+
+marmita_vegana <- function() {
+  html <- "https://www.beleaf.com.br/cardapio-marmitas-veganas" %>%
+    xml2::read_html()
+
+  url <- html %>%
+    xml2::xml_find_all("//a[contains(@class, 'action more')]") %>%
+    purrr::map(~ xml2::xml_attr(.x, "href")) %>%
+    purrr::map_dfr(~ tibble::enframe(purrr::set_names(.x, "url")), .id = "item") %>%
+    dplyr::select("url" = value)
+
+  nm <- c("nome", "descricao", "preco")
+  infos <-
+    html %>% xml2::xml_find_all("//div[contains(@class, 'product-item-details')]") %>%
+    purrr::map(~ stringr::str_squish(xml2::xml_text(xml2::xml_children(.x)))) %>%
+    purrr::map_dfr(~ tibble::enframe(purrr::set_names(.x, nm)), .id = "item") %>%
+    tidyr::pivot_wider() %>%
+    dplyr::mutate(
+      preco = readr::parse_number(preco, locale = readr::locale(decimal_mark = ",")),
+      tipo_refeicao = dplyr::case_when(
+        stringr::str_detect(nome, "BOLO|MOUSSE|BROWNIE|CHEESECAKE|TORTA") ~ "Doce",
+        stringr::str_detect(nome, "CREME DE|MINESTRONE") ~ "Sopa",
+        TRUE ~ "Prato"
+      )
+    )
+
+
+  # dplyr::bind_cols(infos, url)
+}
+
+
+usethis::use_data(marmita_vegana, overwrite = TRUE)
